6ce6532e1cb5e4fb35be517ab8b34e6cd01907ff
Nadia Melissa Martinez Rocha <a2024112081@estudiantes.upsa.edu.bo>
2025-05-08 09:28:42 -0400

    Create Proyecto.cpp
    Archivo principal

---
+++ Proyecto.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Material.h"
+#include "Persistenceutils.h"
+#include "Vector.h"
+#include "Queue.h"
+#include "PriorityQueue.h"
+#include "PersitenciaMateriales.h"
+using namespace std;
+
+
+int main() {
+    Vector<Material> materialesVector;
+    Queue<Material> materialesQueue;
+    PriorityQueue<Material> materialesPQ;
+
+    cargarEnVector(materialesVector);
+    cout << endl;
+
+    cargarEnQueue(materialesQueue);
+    cout << endl;
+
+    cargarEnPriorityQueue(materialesPQ);
+    cout << endl;
+
+    return 0;
+}}
